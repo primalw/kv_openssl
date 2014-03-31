2fe57189acf3ed31928dae4be6c0ef754e199c61
Primal Wijesekera <primalw@gmail.com>
2014-03-31 03:12:46 -0700

    Int:final

--- PythonScript/ecdh_low.h
+++ PythonScript/ecdh_low.h
@@ -5988,34 +5988,6 @@ typedef struct {
 	} FUNCTION;
 
 /********** Headers **********/ 
-/* file: ecdh_low : /Volumes/work/Phd/ECDH/kv_openssl/PythonScriptecdh_low.h */
-unsigned char *ecdh_low(size_t *secret_len) {
-	EC_KEY *key, *peerkey;
-	int field_size;
-	unsigned char *secret;
-	
-	/* Create an Elliptic Curve Key object and set it up to use the ANSI X9.62 Prime 256v1 curve */
- if(NULL == (key = EC_KEY_new_by_curve_name(NID_X9_62_prime256v1))) handleErrors(); 	
-	/* Generate the private and public key */
- if(1 != EC_KEY_generate_key(key)) handleErrors(); 	
-	/* Get the peer's public key, and provide the peer with our public key -
-	 * how this is done will be specific to your circumstances */
- peerkey = get_peerkey_low(key); 	
-	/* Calculate the size of the buffer for the shared secret */
- field_size = EC_GROUP_get_degree(EC_KEY_get0_group(key));  *secret_len = (field_size+7)/8; 	
-	/* Allocate the memory for the shared secret */
- if(NULL == (secret = OPENSSL_malloc(*secret_len))) handleErrors(); 	
-	/* Derive the shared secret */
- *secret_len = ECDH_compute_key(secret, *secret_len, EC_KEY_get0_public_key(peerkey), 								   key, NULL);
-	
-	/* Clean up */
- EC_KEY_free(key);  EC_KEY_free(peerkey); 	
- if(*secret_len <= 0) 	{
-  OPENSSL_free(secret); 		return NULL;
-	}
-	
-	return secret;
-}
 /* file: EC_KEY_new_by_curve_name : /Volumes/work/Phd/ECDH/kv_openssl/crypto/ecec.h */
 #ifndef HEADER_EC_H
 EC_KEY *EC_KEY_new_by_curve_name(int nid);
